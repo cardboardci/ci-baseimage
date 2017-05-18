51be7bacf7e5ee483ebe4f2a89dde1c2db8122ef
jrbeverly <jrbeverly@live.com>
2017-05-18 17:24:51 -0400

    Adding the first stage of gitlab-ci

--- tests/test_alpine-3.5.sh
+++ tests/test_alpine-3.5.sh
@@ -6,9 +6,28 @@
 SCRIPT=$(readlink -f "$0")
 DIR="$(dirname $SCRIPT)"
 ROOT_DIR="$(dirname $DIR)"
-export IMAGE=jrbeverly/baseimage:alpine-3.5
+IMAGE_ARG="$1"
+export IMAGE="${IMAGE_ARG:-jrbeverly/baseimage:alpine}"
 
 # Tests
 #
 # Run tests
 sh ${ROOT_DIR}/tests/tests_alpine.sh
+
+(
+    [[ "${BASH_SOURCE[0]}" == "${0}" ]] || exit 0
+    function assertEquals()
+    {
+        msg=$1; shift
+        expected=$1; shift
+        actual=$1; shift
+        /bin/echo -n "$msg: "
+        if [ "$expected" != "$actual" ]; then
+            echo "FAILED: EXPECTED=$expected ACTUAL=$actual"
+        else
+            echo PASSED
+        fi
+    }
+
+    # Tests specific to this image.
+)
