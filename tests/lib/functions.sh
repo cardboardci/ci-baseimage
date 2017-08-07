9972eb87ac6ac51c925d8eb24b14c5f7eb4c6aba
jrbeverly <jrbeverly@live.com>
2017-08-07 00:42:41 +0000

    Upstream

---
+++ tests/lib/functions.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Tests
+#
+# A set of common functions that should be tested on the docker image.
+
+is_empty_apk()
+{
+    ls -1 /var/cache/apk | wc -l >/dev/null 2>&1  
+}
+
+is_empty_apt()
+{
+    ls -1 /var/lib/apt/lists/ | wc -l >/dev/null 2>&1  
+}
+
+install_apk()
+{
+    apk add --update zip >/dev/null 2>&1
+}
+
+install_apt()
+{
+    apt-get update  >/dev/null 2>&1  
+    apt-get install -y zip  >/dev/null 2>&1  
+}
